2a06f1864a6bf654a08a90bd80a3c578fc04ab81
llNonlosoll <wishmasterar@gmail.com>
2023-04-09 23:58:16 +0200

    first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+// Створюємо селектори для відстеження DOM
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+// Створюємо перемінну для зберігання часу інтервалу та самого інтервалу
+const INTERVAL = 1000;
+let colorInterval = null;
+
+// Генератор випадкового кольору
+function getRandomHexColor() {
+  return (refs.body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  )
+    .toString(16)
+    .padStart(6, 0)}`);
+}
+
+// Додаємо слухачів подій
+refs.start.addEventListener('click', onClickStart);
+refs.stop.addEventListener('click', onClickStop);
+
+// Функція для запуску зміни кольорів та робимо кнопку "Start" неактивною
+function onClickStart() {
+  refs.start.setAttribute('disabled', true);
+  colorInterval = setInterval(() => {
+    getRandomHexColor();
+  }, INTERVAL);
+}
+
+// Функція для зупинення зміни кольорів та робимо кнопку "Start" знову активною
+function onClickStop() {
+  clearInterval(colorInterval);
+  refs.start.removeAttribute('disabled');
+}
