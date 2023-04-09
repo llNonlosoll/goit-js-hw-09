8a6ea60730e47761760a8e473124dfb45e12993b
llNonlosoll <117765352+llNonlosoll@users.noreply.github.com>
2023-04-09 21:58:50 +0000

    Deploying to gh-pages from @ llNonlosoll/goit-js-hw-09@2a06f1864a6bf654a08a90bd80a3c578fc04ab81 🚀

---
+++ 01-color-switcher.6773349c.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")},e=null;t.start.addEventListener("click",(function(){t.start.setAttribute("disabled",!0),e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.6773349c.js.map
