e337c9e6337eb7cbb421eb1f667afb8b2dfa0d84
llNonlosoll <wishmasterar@gmail.com>
2023-04-10 20:04:44 +0200

    some fixes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,8 +6,10 @@ const refs = {
 };
 
 // Створюємо змінну для зберігання часу інтервалу та самого інтервалу
+// та робимо кнопку "Stop" неактивною за замовченням
 const INTERVAL = 1000;
 let colorInterval = null;
+refs.stop.setAttribute('disabled', true);
 
 // Генератор випадкового кольору
 function getRandomHexColor() {
@@ -25,6 +27,7 @@ refs.stop.addEventListener('click', onClickStop);
 // Функція для запуску зміни кольорів та робимо кнопку "Start" неактивною
 function onClickStart() {
   refs.start.setAttribute('disabled', true);
+  refs.stop.removeAttribute('disabled');
   colorInterval = setInterval(() => {
     getRandomHexColor();
   }, INTERVAL);
@@ -34,4 +37,5 @@ function onClickStart() {
 function onClickStop() {
   clearInterval(colorInterval);
   refs.start.removeAttribute('disabled');
+  refs.stop.setAttribute('disabled', true);
 }
