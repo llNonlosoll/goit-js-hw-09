0d7b9bf35a0eaeb697ec13460b0806f9fc1bedd1
llNonlosoll <117765352+llNonlosoll@users.noreply.github.com>
2023-04-10 18:05:14 +0000

    Deploying to gh-pages from @ llNonlosoll/goit-js-hw-09@e337c9e6337eb7cbb421eb1f667afb8b2dfa0d84 🚀

--- 01-color-switcher.0a138045.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]")};let e=null;t.start.addEventListener("click",(function(){t.start.setAttribute("disabled",!0),e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.0a138045.js.map
